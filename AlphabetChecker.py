baa2bdaedc87b0238c86f04298bbd0cb1e61cb5d
Pristql <luigimansion111@gmail.com>
2026-05-11 15:40:34 +0530

    Add files via upload

---
+++ AlphabetChecker.py
@@ -0,0 +1,10 @@
+b = input("Please enter any alphabet: ")
+
+def a():
+    if b in ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"):
+        print("letter is an alphabet")
+    else:
+        print("letter is not an alphabet")
+
+a()
